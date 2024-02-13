0150138044591bfcdd0a22983f69ff57c38b079f
VijayMayhul <vijaymayhul@gmail.com>
2024-02-13 20:09:39 +0530

    Updating day3 tasks

--- Zen_Day3_Q2_showAllCountryFlag.js
+++ Zen_Day3_Q2_showAllCountryFlag.js
@@ -17,6 +17,6 @@ request.onload = function (){
 
     console.log("All Country flags :");
     for(let i =0;i<result.length;i++){
-        console.log(result[i].flags);
+        console.log(result[i].flags.svg);
     }
 }
